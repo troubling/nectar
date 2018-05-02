277f7a62a40fe00d94a87529d4a4316f50a1021a
Greg Holt <greg@gregoryholt.com>
2018-05-02 10:35:04 -0700

    HUM-772 HEAD after auth to prime auth pipelines

--- clientimpl.go
+++ clientimpl.go
@@ -507,5 +507,9 @@ func (c *userClient) authenticate() *http.Response {
 			resp = c.authenticatev1()
 		}
 	}
+	resp2 := c.HeadAccount(nil)
+	if resp2.StatusCode/100 != 200 {
+		return resp2
+	}
 	return resp
 }
